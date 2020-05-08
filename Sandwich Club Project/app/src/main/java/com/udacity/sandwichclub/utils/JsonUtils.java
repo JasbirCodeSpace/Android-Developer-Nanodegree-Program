69f71cfd2ea8b7f84f3a5ed2013b646ff2457c9f
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-08 10:57:21 +0530

    parsing JSON data and return sandwich object

--- Sandwich Club Project/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ Sandwich Club Project/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class JsonUtils {
 
     private static final String TAG = JsonUtils.class.getName();
+
     public static Sandwich parseSandwichJson(String json) {
         final String KEY_NAME = "name";
         final String KEY_MAIN_NAME = "mainName";
@@ -22,35 +23,38 @@ public class JsonUtils {
         final String KEY_IMAGE = "image";
         final String KEY_INGREDIENTS = "ingredients";
 
-        String sandwichName,sandwichOrigin,sandwichDescription,sandwichImage;
-        List<String> sandwichAlsoKnown,sandwichIngredients;
-        try{
+        String sandwichName, sandwichOrigin, sandwichDescription, sandwichImage;
+        List<String> sandwichAlsoKnown, sandwichIngredients;
+        try {
             JSONObject sandwichObject = new JSONObject(json);
-            sandwichName = sandwichObject.getJSONObject(KEY_NAME).getString(KEY_MAIN_NAME);
+
+            Log.v(TAG, sandwichObject.toString());
+            JSONObject nameObject = sandwichObject.getJSONObject(KEY_NAME);
+            sandwichName = nameObject.getString(KEY_MAIN_NAME);
 
             sandwichOrigin = sandwichObject.getString(KEY_PLACE_OF_ORIGIN);
             sandwichDescription = sandwichObject.getString(KEY_DESCRIPTION);
-            sandwichAlsoKnown = jsonArrayToList(sandwichObject.getJSONArray(ALSO_KNOWN_AS));
+            sandwichAlsoKnown = jsonArrayToList(nameObject.getJSONArray(ALSO_KNOWN_AS));
             sandwichIngredients = jsonArrayToList(sandwichObject.getJSONArray(KEY_INGREDIENTS));
             sandwichImage = sandwichObject.getString(KEY_IMAGE);
-            return new Sandwich(sandwichName,sandwichAlsoKnown,sandwichOrigin,sandwichDescription,sandwichImage,sandwichIngredients);
-        }catch (Exception e){
+            return new Sandwich(sandwichName, sandwichAlsoKnown, sandwichOrigin, sandwichDescription, sandwichImage, sandwichIngredients);
+        } catch (Exception e) {
             e.printStackTrace();
-            Log.v(TAG,"Error while reading json data from strings.xml");
+            Log.v(TAG, "Error while reading json data from strings.xml");
         }
 
-    return null;
+        return null;
     }
 
-    public static List<String> jsonArrayToList(JSONArray array){
+    public static List<String> jsonArrayToList(JSONArray array) {
         List<String> list = new ArrayList<>();
         try {
-            for (int i = 0; i <array.length(); i++) {
+            for (int i = 0; i < array.length(); i++) {
                 list.add(array.getString(i));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            Log.e(TAG,e.getMessage());
+            Log.e(TAG, e.getMessage());
         }
 
         return list;
